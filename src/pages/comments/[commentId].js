ad8d25aa565b8ad2cf02907dbb6313f0c543eb89
Maram Hasan <mhassan@itgsoftware.com>
2023-08-14 17:17:48 +0300

    catch all routs & pre-rendering + APIs

---
+++ src/pages/comments/[commentId].js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {comments} from '../../data/index'
+
+export default function CommentDetails({comment}) {
+    return (
+        <React.Fragment>
+            {comment.id} - {comment.text}
+        </React.Fragment>
+    )
+}
+
+// eslint-disable-next-line func-style
+export async function getStaticPaths() {
+    return {
+        paths: [
+            {params: {
+                commentId: '1'
+            }},
+            {params: {
+                commentId: '2'
+            }},
+            {params: {
+                commentId: '3'
+            }},
+        ],
+        fallback: false
+    }
+}
+
+// eslint-disable-next-line func-style
+export async function getStaticProps(context) {
+    const {params} = context
+    const {commentId} = params
+    console.log('maram', commentId)
+    const comment = comments.find((comment) => comment.id === parseInt(commentId))
+    return {
+        props: {
+            comment
+        }
+    }
+}
