af802a91598ef078e42521db29e6142a71703b3f
Maram Hasan <mhassan@itgsoftware.com>
2023-08-16 13:32:54 +0300

    environment variables

--- src/pages/blog/[blogId].js
+++ src/pages/blog/[blogId].js
@@ -8,13 +8,15 @@ export default function Blog({title, description}) {
                 <title>{title}</title>
                 <meta name="description" content={description} />
             </Head>
-            <h1>Article</h1>
+            <h1>Article for {process.env.NEXT_PUBLIC_NAME}</h1>
         </>
     )
 }
 
 // eslint-disable-next-line func-style
 export async function getServerSideProps() {
+    // const user = process.env.DB_USER
+    // const password = process.env.DB_PASSWORD
     return {
         props: {
             title: 'Article Title',
